72e5e5198161e8c8ced62b6a45e8f91ea331b171
Andre-nemesis <105876843+Andre-nemesis@users.noreply.github.com>
2023-02-28 15:38:00 -0300

    Add files via upload

---
+++ quest 2.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+	char str[500];
+	char *point = str;
+	int tam;
+	
+	printf("Digite uma frase: ");
+	gets(str);
+	
+	tam = strlen(str);
+	
+	for (int i = 0; i < tam ; i++) {
+		if (point[i] == 'a' || point[i] == 'e' || point[i] == 'i' || point[i] == 'o' || point[i] == 'u' || point[i] == 'A' || point[i] == 'E' || point[i] == 'I' || point[i] == 'O' || point[i] == 'U') {}
+		else printf("%c", *(point + i));
+		
+		if (point[i] = '\0') break;
+	}
+	
+	return 0;
+}
+
