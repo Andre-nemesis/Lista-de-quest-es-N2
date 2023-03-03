f4ba72a1838ebcd62850d0dab4713591345f6545
Andre-nemesis <105876843+Andre-nemesis@users.noreply.github.com>
2023-03-03 16:45:46 -0300

    Update quest 2.cpp

--- quest 2.cpp
+++ quest 2.cpp
@@ -12,11 +12,18 @@ int main() {
 	
 	tam = strlen(str);
 	
+	int copia;
+	char copiatxt[tam];
+	
 	for (int i = 0; i < tam ; i++) {
 		if (point[i] == 'a' || point[i] == 'e' || point[i] == 'i' || point[i] == 'o' || point[i] == 'u' || point[i] == 'A' || point[i] == 'E' || point[i] == 'I' || point[i] == 'O' || point[i] == 'U') {}
-		else printf("%c", *(point + i));
+		else {
+			copiatxt[copia] = point[i];
+			copia++;
+		}
 	}
+
+	for (int i = 0;i < copia;i++) printf("%c",copiatxt[i]);
 	
 	return 0;
 }
-
